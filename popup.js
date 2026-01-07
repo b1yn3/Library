615a7e783f8d664a678877f0a19b969468a8312e
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-01-08 00:05:07 +0800

    chore: remove leftover test line

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
 const popup = document.getElementById("popup");
 const openBtn = document.getElementById("openPopup");
 const closeBtn = document.getElementById("closePopup");
-const submitBtn = document.getElementById("submitAnswer");
 
 openBtn.onclick = () => {
     popup.style.display = "block";
