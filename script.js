af621989b47dad96bcb84834ca48ff62dc2575de
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-01-14 15:39:43 +0800

    chore: add preliminary function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const popup = document.getElementById("popup");
+const openBtn = document.getElementById("openPopup");
+const closeBtn = document.getElementById("closePopup");
+const addBook = document.getElementById("saveBook");
+
 const myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -10,3 +15,24 @@ function Book(title, author, pages, read){
 function addBookToLibrary(){
 
 }
+
+openBtn.onclick = () => {
+    popup.style.display = "block";
+};
+
+closeBtn.onclick = () => {
+    popup.style.display = "none";
+};
+
+addBook.onclick = () => {
+    const newAuthor = document.getElementById("author");
+    const newTitle = document.getElementById("title");
+    const newPage = document.getElementById("page");
+    const newRead = document.getElementById("read");
+    const form = document.querySelector(".modal-content")
+    const book1 = new Book(newAuthor.value, newTitle.value, newPage.value, newRead.value);
+    myLibrary.push(book1);
+    console.log(`Book Added: ${book1}`);
+    popup.style.display = "none";
+    form.reset()
+};
