5c4b64d48624b9b7eb00e1b6302010d9fc9a9842
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-01-19 20:48:02 +0800

    feat: add id property to Book constructor

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read, id){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = id;
 }
 
 function addBookToLibrary(){
