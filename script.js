32d1b2b7ef6382d9281dd6117e4dd1b4cc0562e1
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-01-19 21:53:06 +0800

    feat: show Add Book dialog when clicking Add Book button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const myLibrary = [];
 
+const showDialog = document.getElementById('showDialog');
+const confirmDialog = document.getElementById('confirmDialog');
+
 function Book(title, author, pages, read, id){
     this.title = title;
     this.author = author;
@@ -11,3 +14,7 @@ function Book(title, author, pages, read, id){
 function addBookToLibrary(){
 
 }
+
+showDialog.addEventListener('click', () => {
+    confirmDialog.showModal();
+});
