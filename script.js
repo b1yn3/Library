f8bf7b191d1cc30108c289bde87caae02346530c
Bryne <228299496+b1yn3@users.noreply.github.com>
2026-02-09 17:55:57 +0800

    feat: render book card in DOM when adding a book

--- script.js
+++ script.js
@@ -20,6 +20,39 @@ function addBookToLibrary(){
     Book(titleInput.value, authorInput.value, ageInput.value, selectInput.value, );
 }
 
+function addBookToHTML(titleInput, authorInput, pageInput, selectInput, uuid){
+    const article = document.createElement('article');
+    article.id = uuid;
+
+    const div = document.createElement('div');
+    div.classList.add('delete');
+    const button = document.createElement('button');
+    const img = document.createElement('img');
+    img.src = 'icon/image.png';
+    button.appendChild(img);
+    div.appendChild(button);
+
+    const h4 = document.createElement('h4');
+    h4.classList.add('bookTitle');
+    h4.textContent = titleInput;
+    const pAuthor = document.createElement('p');
+    pAuthor.classList.add('bookAuthor');
+    pAuthor.textContent = authorInput;
+
+    const pPage = document.createElement('p');
+    pPage.classList.add('bookPages');
+    pPage.textContent = pageInput;
+
+    const pSelect = document.createElement('p');
+    pSelect.classList.add('bookRead');
+    pSelect.textContent = selectInput;
+
+    article.append(div, h4, pAuthor, pPage, pSelect);
+
+    const main = document.querySelector('Main');
+    main.appendChild(article);
+}
+
 showDialog.addEventListener('click', () => {
     confirmDialog.showModal();
 });
@@ -38,6 +71,7 @@ dialogForm.addEventListener("submit", function(e) {
     console.log(`Title: ${titleInput.value}, ${authorInput.value} ${pageInput.value} ${selectInput.value} ${uuid}`);
 
     myLibrary.push(new Book(titleInput.value, authorInput.value, pageInput.value, selectInput.value, uuid));
+    addBookToHTML(titleInput.value, authorInput.value, pageInput.value, selectInput.value, uuid);
     console.log(myLibrary);
-
+    dialogForm.reset();
 });
